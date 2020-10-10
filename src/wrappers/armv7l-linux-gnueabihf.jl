1e05931eee5530dcadaa521492c9e8eededb95af
jlbuild <juliabuildbot@gmail.com>
2020-10-10 01:34:09 +0000

    libass_jll build 0.14.0+4

--- src/wrappers/armv7l-linux-gnueabihf.jl
+++ src/wrappers/armv7l-linux-gnueabihf.jl
@@ -5,52 +5,15 @@ using FreeType2_jll
 using FriBidi_jll
 using Bzip2_jll
 using Zlib_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "LD_LIBRARY_PATH"
-LIBPATH_default = ""
-
-# Relative path to `libass`
-const libass_splitpath = ["lib", "libass.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libass_path = ""
-
-# libass-specific global declaration
-# This will be filled out by __init__()
-libass_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libass = "libass.so.9"
-
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("libass")
+JLLWrappers.@declare_library_product(libass, "libass.so.9")
 function __init__()
-    global artifact_dir = abspath(artifact"libass")
+    JLLWrappers.@generate_init_header(FreeType2_jll, FriBidi_jll, Bzip2_jll, Zlib_jll)
+    JLLWrappers.@init_library_product(
+        libass,
+        "lib/libass.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (FreeType2_jll.PATH_list, FriBidi_jll.PATH_list, Bzip2_jll.PATH_list, Zlib_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (FreeType2_jll.LIBPATH_list, FriBidi_jll.LIBPATH_list, Bzip2_jll.LIBPATH_list, Zlib_jll.LIBPATH_list,))
-
-    global libass_path = normpath(joinpath(artifact_dir, libass_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libass_handle = dlopen(libass_path)
-    push!(LIBPATH_list, dirname(libass_path))
-
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ':')
-    global LIBPATH = join(vcat(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ':')
-
-    
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
-
